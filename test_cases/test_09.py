991a356394485c7714f2379b4ad751e7280045a8
cindy <xxy1115@163.com>
2022-10-25 16:25:37 +0800

    自动化代码上传

--- test_cases/test_09.py
+++ test_cases/test_09.py
@@ -24,8 +24,8 @@ from utils.yamlControl import parse_yaml
 
 
 class TestCase1:
-    token = "caba18d8dc5b4a47afb54f3fb2eab0c0"
-    user_id = 1681
+    token = ""
+    user_id = 0
     dicts = {}
     user_info = {}
     product_type = []  # 用户产品类别权限列表
@@ -39,7 +39,7 @@ class TestCase1:
         pass
 
     @allure.title("登录")
-    def atest_1(self):
+    def test_1(self):
         res = Login().login(self.test_data["user"]["user01"])
         pytest.assume(res, "登录失败")
         TestCase1.token = res["token"]
